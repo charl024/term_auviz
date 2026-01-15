2b7831f43ea904ab119e21eb8c09f952f017c53c
charl024 <comaoeng2003@gmail.com>
2026-01-15 11:22:41 -0700

    added ringbuffer read/write

--- include/ringbuffer.h
+++ include/ringbuffer.h
@@ -14,5 +14,7 @@ typedef struct {
 
 ringbuffer_t* ringbuffer_create();
 void ringbuffer_destroy(ringbuffer_t* buffer);
+void ringbuffer_read(ringbuffer_t* buffer, void* output_data, size_t output_size);
+void ringbuffer_write(ringbuffer_t* buffer, void* input_data, size_t input_size);
 
 #endif
