a49dd9394cb30d373142b9825a55546c4aa52eac
charl024 <comaoeng2003@gmail.com>
2026-01-15 19:03:37 -0700

    filled in ringbuffer initiliazation method, ringbuffer read

--- include/ringbuffer.h
+++ include/ringbuffer.h
@@ -2,14 +2,16 @@
 #define RINGBUFFER_H
 
 #include <stdlib.h>
+#include <pthread.h>
 
 #define RINGBUFFER_SIZE 4096
 
 typedef struct {
-    float *buffer;
+    float *data;
     size_t size;
     size_t write_index;
     size_t read_index;
+    pthread_mutex_t *mutex;
 } ringbuffer_t;
 
 ringbuffer_t* ringbuffer_create();
--- src/ringbuffer.c
+++ src/ringbuffer.c
@@ -1,1 +1,62 @@
 #include "ringbuffer.h"
+
+ringbuffer_t* ringbuffer_create() 
+{
+    ringbuffer_t* buffer = (ringbuffer_t*)calloc(1, sizeof(ringbuffer_t));
+
+    if (!buffer) {
+        fprintf(stderr, "Error allocating ringbuffer_t./n");
+        return NULL;
+    }
+
+    buffer->data = (float*)malloc(sizeof(float) * RINGBUFFER_SIZE);
+
+    if (!buffer) {
+        fprintf(stderr, "Error allocating ringbuffer_t data./n");
+        return NULL;
+    }
+
+    pthread_mutex_init(buffer->mutex, NULL);
+    
+    buffer->size = size;
+    buffer->write_index = 0;
+    buffer->read_index = 0;
+
+    return buffer;
+}
+
+void ringbuffer_destroy(ringbuffer_t* buffer) 
+{
+    if (!buffer) {
+        fprintf(stderr, "Error destroying ringbuffer_t, uninitialized./n");
+        return;
+    }
+
+    if (!buffer->data) {
+        fprintf(stderr, "Error allocating ringbuffer_t, data error./n");
+        return;
+    }
+
+    free(buffer->data);
+    free(buffer);
+}
+
+void ringbuffer_read(ringbuffer_t* buffer, void* output_data, size_t output_size) 
+{
+    size_t r, w;
+    pthread_mutex_lock(buffer->mutex);
+    r = buffer->read_index;
+    w = buffer->write_index;
+    pthread_mutex_unlock(buffer->mutex);
+
+    size_t available = w - r;
+    size_t max_n = available < output_size ? available : output_size;
+
+    // TODO read buffer data into output
+
+}
+
+void ringbuffer_write(ringbuffer_t* buffer, void* input_data, size_t input_size) 
+{
+
+}
