27602c04b52ebc7c4f77cac19d9fa50dc06e45b4
charl024 <comaoeng2003@gmail.com>
2026-01-20 21:22:11 -0700

    fixed includes, added run function to launch capture thread

--- include/pipewire_backend.h
+++ include/pipewire_backend.h
@@ -1,17 +1,22 @@
 #ifndef PIPEWIRE_BACKEND_H
 #define PIPEWIRE_BACKEND_H
 
+#include <bits/pthreadtypes.h>
 #include <pipewire/pipewire.h>
-#include <spa/param/audio/format-util.h>
-#include <math.h>
+#include <spa/param/audio/format-utils.h>
+#include <pthread.h>
+#include "ringbuffer.h"
 
 typedef struct {
 	struct pw_main_loop *loop;
 	struct pw_stream *stream;
+	struct spa_pod_builder *spap_builer;
 	ringbuffer_t *ringbuffer;
+	pthread_t *cap_thread;
 } pipewire_capture_t;
 
 pipewire_capture_t* pipewire_capture_create();
+void pipewire_capture_run(pipewire_capture_t* capture);
 void pipewire_capture_destroy(pipewire_capture_t* capture);
 int pipewire_capture_get_audio(pipewire_capture_t* capture, float* buffer, size_t size);
 
