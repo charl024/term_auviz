94c89f8e55cf61121e41a96ab4f8573c75431ed7
charl024 <comaoeng2003@gmail.com>
2026-01-20 21:23:59 -0700

    added static non-api functions for pipewire-related processes

--- src/pipewire_backend.c
+++ src/pipewire_backend.c
@@ -1,7 +1,31 @@
 #include "pipewire_backend.h"
 #include "proj_defines.h"
+#include <pthread.h>
 
-pipewire_capture_t* pipewire_capture_init() 
+static void on_process(void *data) 
+{
+
+}
+    
+
+static const struct pw_stream_events stream_events = {
+    PW_VERSION_STREAM_EVENTS,
+    .process = on_process,
+};
+
+static void* pipewire_capture_launch_loop(void* data) 
+{
+    pipewire_capture_t* capture = (pipewire_capture_t*)data;
+    pw_main_loop_run(capture->loop);
+}
+
+void pipewire_capture_run(pipewire_capture_t* capture) 
+{
+    // create pthread that runs pw loop
+    pthread_create(capture->cap_thread, NULL, pipewire_capture_launch_loop, (void*)capture);
+}
+
+pipewire_capture_t* pipewire_capture_create() 
 {
     pipewire_capture_t* capture = malloc(sizeof(pipewire_capture_t));
     if (!capture) {
@@ -22,11 +46,36 @@ pipewire_capture_t* pipewire_capture_init()
 			props,
 			&stream_events,
 			&capture);
-	
+	// initialize ringbuffer
     capture->ringbuffer = ringbuffer_create(FFT_SIZE);
 
+    struct spa_pod_builder builder = SPA_POD_BUILDER_INIT(capture->ringbuffer->data, capture->ringbuffer->size);
+    const struct spa_pod *params[1];
 
+    // parameter setup
+    params[0] = spa_format_audio_raw_build(
+        &builder,
+        SPA_PARAM_EnumFormat,
+        &SPA_AUDIO_INFO_RAW_INIT(
+            .format   = SPA_AUDIO_FORMAT_F32,
+            .rate     = 48000,
+            .channels = 2
+        ));
+
+    // connect pw stream with parameters
+    pw_stream_connect(
+        capture->stream,
+        PW_DIRECTION_INPUT,
+        114,
+        PW_STREAM_FLAG_AUTOCONNECT |
+        PW_STREAM_FLAG_MAP_BUFFERS |
+        PW_STREAM_FLAG_RT_PROCESS,
+        params, 1);
     
+    // allocate thread
+    capture->cap_thread = (pthread_t*)malloc(sizeof(pthread_t));
+    
+    // return the capture struct, do not start the pw loop
     return capture;
 }
 
