299ffbb9f7dff19151c580b0d92be348b9467c3d
charl024 <comaoeng2003@gmail.com>
2026-01-20 21:48:55 -0700

    removed incorrect header

--- include/pipewire_backend.h
+++ include/pipewire_backend.h
@@ -1,9 +1,9 @@
 #ifndef PIPEWIRE_BACKEND_H
 #define PIPEWIRE_BACKEND_H
 
-#include <bits/pthreadtypes.h>
 #include <pipewire/pipewire.h>
 #include <spa/param/audio/format-utils.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include "ringbuffer.h"
 
@@ -12,6 +12,8 @@ typedef struct {
 	struct pw_stream *stream;
 	struct spa_pod_builder *spap_builer;
 	ringbuffer_t *ringbuffer;
+
+	int thread_running;
 	pthread_t *cap_thread;
 } pipewire_capture_t;
 
